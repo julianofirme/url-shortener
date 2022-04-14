618d0428a42eec835d51c756787cf5c408ed44d5
Juliano Firme <julianofirme1@gmail.com>
2022-04-14 00:44:09 -0300

    feat: generating the url hash and save on array (for now)

--- src/api/routes/app.routes.ts
+++ src/api/routes/app.routes.ts
@@ -1,23 +1,29 @@
 import { Router } from "express";
+import shortid from "shortid";
+import { Url } from "../../@types/Url";
 import { validateUrl } from "../utils";
 
 export const router = Router();
+
+const baseUrl = 'http://localhost:3030'
+
+const urls: Url[] = []
 
 router.post('/urls', async (req, res) => {
   const { original_url } = req.body;
   const isValidUrl = validateUrl(original_url);
 
   if (!isValidUrl) {
-    res.status(422).json({ err: "Invalid url" });
+    res.status(422).json({ error: "Invalid url" });
   }
 
-  res.sendStatus(200);
-})
+  const hash = shortid.generate();
 
-router.get('/urls/:hash', async (req, res) => {
-  const hash = req.params.hash;
-  console.log(hash);
-})
+  const shortUrl = `${baseUrl}/${hash}`
 
+  urls.push({ original_url, hash })
+
+  res.status(200).json({ url: shortUrl })
+})
 
 export default router;
