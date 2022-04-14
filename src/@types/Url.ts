9da56cd02d2efa001888fe0e3a7e6d064e591bea
Juliano Firme <julianofirme1@gmail.com>
2022-04-14 00:41:25 -0300

    fix: url types

--- src/@types/Url.ts
+++ src/@types/Url.ts
@@ -1,17 +1,4 @@
-export interface PostUrl {
-  user_id: string;
-  original_url: string;
-}
-
-export interface GetUrl {
-  original_url: string;
-  hash: string;
-}
-
 export interface Url {
-  uuid: string;
   original_url: string;
-  user_id: string;
   hash: string;
-  created_at: Date;
 }
