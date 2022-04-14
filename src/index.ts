5e0a70236853404606dd19127c9e3f5f9159fdf7
Juliano Firme <julianofirme1@gmail.com>
2022-04-13 23:52:33 -0300

    fix: app server

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import express, { Application } from 'express'
 import router from './api/routes/app.routes';
 
 const app: Application = express();
+const PORT = process.env.PORT || 3030;
+ 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api", router)
+
+app.listen(PORT, () => {
+  console.log(`:::App is listening on port ${PORT}!`)
+})
+
