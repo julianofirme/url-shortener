de138726216ff354d81fed382a1b937cb0359d43
Juliano Firme <julianofirme1@gmail.com>
2022-04-13 23:52:22 -0300

    feat: method to validate if the url is valid

--- src/api/routes/app.routes.ts
+++ src/api/routes/app.routes.ts
@@ -1,15 +1,22 @@
 import { Router } from "express";
+import { validateUrl } from "../utils";
 
 export const router = Router();
 
 router.post('/urls', async (req, res) => {
-  const { origUrl } = req.body;
-  console.log("🚀 ~ origUrl", origUrl)
+  const { original_url } = req.body;
+  const isValidUrl = validateUrl(original_url);
+
+  if (!isValidUrl) {
+    res.status(422).json({ err: "Invalid url" });
+  }
+
+  res.sendStatus(200);
 })
 
 router.get('/urls/:hash', async (req, res) => {
-  const url = req.params.hash;
-  console.log("🚀 ~ url", url)
+  const hash = req.params.hash;
+  console.log(hash);
 })
 
 
