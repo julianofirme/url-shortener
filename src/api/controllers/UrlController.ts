603cd31b6b4c97c9e1ca765b2e21e927dcf55150
Juliano Firme <julianofirme1@gmail.com>
2022-04-17 20:40:26 -0300

    feat: redirect method

--- src/api/controllers/UrlController.ts
+++ src/api/controllers/UrlController.ts
@@ -3,56 +3,50 @@ import shortid from "shortid";
 import { Url } from "../models/Url";
 import { validateUrl } from "../utils";
 
-const baseUrl = 'http://localhost:3030'
+const baseUrl = 'http://localhost:3000'
 
 export class UrlController {
   public router: Router;
 
-    constructor() {
-      this.router = Router()
+  constructor() {
+    this.router = Router()
+  }
+
+  create = async (req: Request, res: Response) => {
+    const { original_url } = req.body;
+    const isValidUrl = validateUrl(original_url);
+
+    if (!isValidUrl) {
+      res.status(422).json({ error: "Invalid url" });
     }
 
-    create = async (req: Request, res: Response) => {
-      const { original_url } = req.body;
-      const isValidUrl = validateUrl(original_url);
-    
-      if (!isValidUrl) {
-        res.status(422).json({ error: "Invalid url" });
+    const hash = shortid.generate();
+    const shortUrl = `${baseUrl}/${hash}`
+
+    await Url.create({ original_url, hash })
+
+    res.status(200).json({ url: shortUrl })
+  }
+
+  getUrl = async (req: Request, res: Response) => {
+    const hash = req.params.hash
+
+    if (!hash) {
+      res.status(404).json({ error: "Not found url" });
+    }
+
+    const sqlResponse = await Url.findOne({
+      where: {
+        hash
       }
-    
-      const hash = shortid.generate();
-      const shortUrl = `${baseUrl}/${hash}`
-    
-      const response = await Url.create({ original_url, hash })
-      console.log("🚀 ~ response", response)
-    
-      res.status(200).json({ url: shortUrl })
+    })
+
+    const url = JSON.stringify(sqlResponse)
+
+    if (!url) {
+      res.status(404).json({ error: "Not found url" });
     }
 
-    getUrl = async (req: Request, res: Response) => {
-      try {
-        const hash = req.params.hash
-    
-        if (!hash) {
-          res.status(404).json({ error: "Not found url" });
-        }
-        
-        const url = await Url.findOne({
-          where: {
-            hash
-          }
-        })
-        
-        if (!url) {
-          res.status(404).json({ error: "Not found url" });
-        }
-        
-        console.log("🚀 ~ url", url)
-
-        // return res.redirect();
-      } catch (err) {
-        console.log(err);
-        res.status(500).json('Server Error');
-      }
-    }
+    return res.redirect(JSON.parse(url).original_url);
+  }
 }
