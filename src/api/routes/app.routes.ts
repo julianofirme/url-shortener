dac5dd5262e9b0831bc19999e59262d33ffcb20d
Juliano Firme <julianofirme1@gmail.com>
2022-04-17 20:24:49 -0300

    refactor: export the get methods to the Url Controller

--- src/api/routes/app.routes.ts
+++ src/api/routes/app.routes.ts
@@ -1,46 +1,13 @@
 import { Router } from "express";
-import shortid from "shortid";
-import { Url } from "../../@types/Url";
-import { validateUrl } from "../utils";
+import { UrlController } from "../controllers/UrlController";
+
 
 export const router = Router();
 
-const baseUrl = 'http://localhost:3030'
+const urlController = new UrlController()
 
-const urls: Url[] = []
+router.post('/urls', urlController.create)
 
-router.post('/urls', async (req, res) => {
-  const { original_url } = req.body;
-  const isValidUrl = validateUrl(original_url);
-
-  if (!isValidUrl) {
-    res.status(422).json({ error: "Invalid url" });
-  }
-
-  const hash = shortid.generate();
-
-  const shortUrl = `${baseUrl}/${hash}`
-
-  urls.push({ original_url, hash })
-
-  res.status(200).json({ url: shortUrl })
-})
-
-router.get('/:hash', async (req, res) => {
-  try {
-    const hash = req.params.hash
-
-    if (!hash) {
-      res.status(404).json({ error: "Not found url" });
-    }
-    
-    const url = urls.filter(url => url.hash == hash)[0]
-
-    return res.redirect(url.original_url);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json('Server Error');
-  }
-});
+router.get('/:hash', urlController.getUrl);
 
 export default router;
