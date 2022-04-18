0a28711956de27644c5fabd2576c29ac610f90f0
Juliano Firme <julianofirme1@gmail.com>
2022-04-17 23:46:49 -0300

    fix: host env

--- src/api/db/index.ts
+++ src/api/db/index.ts
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   `${process.env.POSTGRES_USER}`,
   `${process.env.POSTGRES_PASSWORD}`,
   {
-    host: 'localhost',
+    host: `${process.env.POSTGRES_HOST}`,
     dialect: 'postgres',
     port: Number(process.env.POSTGRES_PORT)
   }
