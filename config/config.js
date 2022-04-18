f8b68140a380547927c2e3449c9a922af82c66ca
Juliano Firme <julianofirme1@gmail.com>
2022-04-17 23:59:15 -0300

    fix: heroku database url

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ module.exports = {
     logging: false,
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
