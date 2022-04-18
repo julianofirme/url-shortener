24e1ac6a64ab5dceff2dc7e3f39c1fc5c277b8e9
Juliano Firme <julianofirme1@gmail.com>
2022-04-18 00:22:56 -0300

    prod db

--- src/api/db/index.ts
+++ src/api/db/index.ts
@@ -3,14 +3,11 @@ import 'dotenv/config'
 
 
 const sequelize = new Sequelize(
-  `${process.env.POSTGRES_DB}`,
-  `${process.env.POSTGRES_USER}`,
-  `${process.env.POSTGRES_PASSWORD}`,
-  {
-    host: `${process.env.POSTGRES_HOST}`,
-    dialect: 'postgres',
-    port: Number(process.env.POSTGRES_PORT)
-  }
+  `${process.env.DATABASE_URL}`, {
+  host: `${process.env.POSTGRES_HOST}`,
+  dialect: 'postgres',
+  port: Number(process.env.POSTGRES_PORT)
+}
 )
 
 async function connection() {
