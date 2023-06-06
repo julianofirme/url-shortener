9cf1523bb2f94b6cc446d962cb8925367c14a762
Juliano <julianofirme23@gmail.com>
2023-06-06 08:50:15 -0300

    feat: allow cors

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ import { connection } from './api/db';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3030;
- 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
-
+app.use(cors({
+  origin: 'https://url-shortener-mu-topaz.vercel.app',
+}));
 
 connection()
 
